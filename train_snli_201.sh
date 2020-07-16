35c11ab8845fc2ea6c46546a7b6d23e7e5b6affd
wangzhipeng <wangzhipeng05@corp.netease.com>
2020-07-16 14:53:00 +0800

    add train.sh

---
+++ train_snli_201.sh
@@ -0,0 +1,13 @@
+python train.py \
+      --data /data/wangzhipeng05/DAMM_data/data \
+      --output /model/wangzhipeng05/DAMM_output/snli \
+      --task snli \
+      --labels [0,1,2] \
+      --num_hidden_layers 4 \
+      --per_gpu_train_batch_size 256 \
+      --per_gpu_eval_batch_size 256 \
+      --per_gpu_test_batch_size 256 \
+      --logging_steps 100 \
+      --save_steps 500 \
+      --eval_steps 150  
+# 88.75
