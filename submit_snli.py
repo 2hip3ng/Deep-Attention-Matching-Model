12900f0287be3b232184936baea0a3f54710e023
wangzhipeng <zhipengwang1121@163.com>
2020-08-09 11:51:27 +0800

    add ablation sh

--- submit_snli.py
+++ submit_snli.py
@@ -9,10 +9,10 @@ worker_config = {
         }
 }
 
-job = TrainJob(entry_point='train_snli_best_no_symmetry.sh',       #训练入口,也可以是sh脚本
+job = TrainJob(entry_point='train_snli_best_no_cnn_no_resnet_no_symmetry.sh',       #训练入口,也可以是sh脚本
             worker_config=worker_config,     # 训练资源配置
             frame_version='pytorch==1.5.0',  #指定框架及版本
             stream_log=True,                 #直接在当前窗口输出训练日志（调试时开启）
-            job_name='wzp-damm-snli-best-no-symmetry')            #训练job名字
+            job_name='wzp-damm-snli-best-no-cnn-no-resnet-no-symmetry')            #训练job名字
 
 job.submit()
